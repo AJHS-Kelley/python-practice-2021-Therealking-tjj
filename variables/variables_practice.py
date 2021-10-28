3261132fd987395f4d7ee44487d33a11bff198d7
Terrance H <71032007+Therealking-tjj@users.noreply.github.com>
2021-10-28 14:10:34 -0400

    Update variables_practice.py

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -3,4 +3,9 @@
 # common data types and variables in python 
 
 life_count = 3 # integer value, all whole numbers, +/- and 0
+account_balance = 4232.57 # floating point value, +/-. 
+user_name = "billy bop joe" # string, a collection  of letters/numbers.
 
+print(life_count)
+print(account_balance)
+print(user_name)
